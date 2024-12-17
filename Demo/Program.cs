86062980d0900992b76109a4943ca83310838d1d
arafa <arafa@ARAFA>
2024-12-17 20:44:09 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+
+
+        //static function (class member function)
+        //object member function
+        public static void Swap(ref int X, ref int Y)
+        //هخليها ref///////
+        {
+            int temp = X;
+            X = Y;
+            Y = X;
+        }
+        public static void SumMul(int X, int Y, out int Sum, out int Mul)
+        {
+            Sum = Y + X;
+            Mul = Y * X;
+        }
+        public static int SumArray(params int[] Arr)
+        { 
+            int sum = 0;
+            if (Arr is not null)
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    sum += Arr[i]; //sum =sum + arr[i]
+
+                }
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            #region judged array
+            // judged array =>array of array
+            //int[][] Numbers = new int[3][];
+            //Numbers[0]=new int[] {1,2,3 };
+            //Numbers[1] = new int[] { 4,5};
+            //Numbers[2] = new int []{ 6};
+            //for (int i = 0; i<Numbers.Length; i++)
+            //{
+            //    for (int j = 0; j < Numbers[i].Length; j++)
+            //    {
+            //        Console.WriteLine(Numbers[i][j]);
+            //    }
+            //}
+
+
+            #endregion
+
+            #region Passing By Value
+            //int A = 5; int B = 6;
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+
+            //Swap(ref A, ref B);
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+
+            #endregion
+
+            #region Passing By Value
+            //int A = 4, B = 5, SumResult , MulResult;
+            //SumMul(B,A,out SumResult,out MulResult);
+            //Console.WriteLine($"the sum={SumResult}");
+            //Console.WriteLine($"the mul={MulResult}");
+            #endregion 
+
+            #region Params
+            //int[] Numbers = { 1, 2, 3, 4, 5, 6, };
+            //int total = SumArray(1,2,3,4,5,6);
+            //Console.WriteLine($"the sum of the array {total}");
+            #endregion
+
+            #region Boxing vs UnBoxing
+            //Boxing=>casting from datatype [value type]to data type[refernce]
+            //un Boxing=>casting from datatype [refernce type]to data type[value]  un safe
+            #region ex1
+            //object obj;
+            ////declare for refernce of type object refering null
+            ////this refernce obj can refer to an instance from type object
+            //obj = new object();
+            //obj = new string("ali");
+            //obj = 'a';
+            //obj = 3;
+            //obj = true;
+            //bool flag = (bool)obj;
+            //Console.WriteLine(flag); //un boxing un safe
+            #endregion
+            #region ex2
+            //int x = 5;
+            //object o1 = x;
+            ////casting from int [value type ] to object [ref]
+            ////5------>new object (5) boxing
+            ////parent=child
+            ////animal=dog
+            ////boxing safe casting // imlicit casting
+            //Console.WriteLine(o1);
+            //unboxing
+            //object o2 = 10;
+            //o2 = "ali";
+            //int y = (int)o2;
+            ////casting from ref to int //explicit
+            ////unboxing unsafe casting
+            ////new object(10)------10
+            ////child=parent
+            //Console.WriteLine(o2);
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region (1)
+
+            //int num = 10;
+            //update(num);
+            //Console.WriteLine(num);  //Not Changes (num = 5)
+            //// 2- Passing by Reference:
+            ////the memory address of the original variable is passed to the method.
+            ////This means that any changes made to the parameter inside the method will
+            ////affect the original variable outside the method.
+            //updatere(ref num);
+            //Console.WriteLine(num); // output(num = 20)
+            #endregion
+
+            #region  (2)  
+
+            //Person person = new Person { Name = "John" };
+            //update(person); // Passing by value
+            //Console.WriteLine("Person's Name after method call: " + person.Name);
+            ////2- Passing Reference Type by Reference:
+            //// the method receives the memory address of the original object.
+            //// This means the method can modify the original reference itself,
+            //// not just the object it points to.
+            //// and will affect the original variable in the calling method
+            //Person person00 = new Person { Name = "John" };
+            //updatere(ref person00); // Passing by reference using 'ref'
+            //Console.WriteLine("Person's Name after method call: " + person.Name); // Output: "Mohamed"
+            #endregion
+
+            #region (3) 
+            //Console.Write("Enter The 1st Number: ");
+            //int.TryParse(Console.ReadLine(), out int num1);
+            //Console.Write("Enter The 2nd Number: ");
+            //int.TryParse(Console.ReadLine(), out int num2);
+            //calculator(num1, num2, out int sum, out int sub);
+            //Console.WriteLine($"{num1} + {num2} = {sum}\n{num1} - {num2} = {sub}");
+            #endregion
+
+            #region (4) 
+            //Console.Write("Enter A Number: ");
+            //int.TryParse(Console.ReadLine(), out int sum);
+            //Console.WriteLine($"The Sum Of The Digits Of The Number {sum}: {sumDigits(sum)}");
+            #endregion
+
+            #region (5) 
+            //Console.Write("Enter A Number: ");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //Console.WriteLine(IsPrime(num));
+            #endregion
+
+            #region (6) 
+            //Console.Write("Enter Size Of The Array: ");
+            //int.TryParse(Console.ReadLine(), out int size);
+            //if (size < 2)
+            //    Console.WriteLine("Invaild Input ");
+            //else
+            //{
+            //    int[] arr = new int[size];
+            //    Console.WriteLine("Enter The Elements Of The Array: ");
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+            //        Console.Write($"Enter Element {i + 1}: ");
+            //        while (!(int.TryParse(Console.ReadLine(), out arr[i])))
+            //            Console.WriteLine("Please Enter An Integer");
+            //    }
+            //    MinMaxArray(arr, out int max, out int min);
+            //    Console.WriteLine($"The Maximum Number: {max}\nThe Minimum Number: {min}");
+            //}
+            #endregion
+
+            #region (7) 
+            //bool flag;
+            //int num;
+            //do
+            //{
+            //    Console.Write("Enter The Number: ");
+            //    flag = int.TryParse(Console.ReadLine(), out  num);
+            //} while (!flag || num == 0);
+            //Console.WriteLine($"Factorial of {num} is: { Factorial(num)}");
+            #endregion
+
+            #region (8) 
+            //Console.Write("Enter The Text: ");
+            //string str = Console.ReadLine();
+            //Console.Write("Enter The Position: ");
+            //int.TryParse(Console.ReadLine(), out int pos);
+            //Console.Write("Enter The New Char: ");
+            //char.TryParse(Console.ReadLine(), out char c);
+            //string newStr = ChangeChar(str, pos, c);
+            //Console.WriteLine($"Original string: {str}");
+            //Console.WriteLine($"Modified string: {newStr}");
+            #endregion
+
+        }
+    }
+}
